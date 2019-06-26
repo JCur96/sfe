1b53433ad89ea22f742e19803cb606900dba4ea1
JCur96 <jake.curry96@gmail.com>
2019-06-26 09:59:06 +0100

    adding changes to sandbox before I clean up

--- R/sandbox.R
+++ R/sandbox.R
@@ -14,14 +14,15 @@
 # it would be exceedingly sensible to just have convex hull calcs just update
 # geometry column, which would streamline several downstream functions, and
 # reduce data volume
-usethis::edit_r_environ()
-devtools::install_github("JCur96/sfe")
-devtools::install_github('ropenscilabs/datastorr')
-# datastorr is not avaliable for R version 3.6.0!
-# so I will have to install an older version to check if this works
-# and implement testing on it obviosuly
-# Should make a note of this in the description file I think
-datastorr::mydata_versions()
-
-datastorr::autogenerate('JCur96/sfe', 'read.csv')
+# usethis::edit_r_environ()
+# devtools::install_github("JCur96/sfe", force = T)
+# devtools::install_github('ropenscilabs/datastorr')
+# # datastorr is not avaliable for R version 3.6.0!
+# # so I will have to install an older version to check if this works
+# # and implement testing on it obviosuly
+# # Should make a note of this in the description file I think
+# datastorr::mydata_versions()
 #
+# noquote(datastorr::autogenerate(repo = 'JCur96/sfe', read = 'read.csv'))
+# #
+# usethis::edit_r_environ()
